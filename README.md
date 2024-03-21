eb0b3f7c925c9ff3cb21976936710c80d37e5e55
Pankaj Kashyap <pankajkashyap2405@gmail.com>
2024-03-21 15:45:50 +0530

    esho

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # MTN Self Service & eShop Project-Automtaion Test Script (Cypress+Typescript+CucumberBDD)
-https://selfservicepreprod.mtn.ng/
+https://selfservicepreprod.mtn.ng/ <br>
+https://shop.mtn.ng/
 by Pankaj
--- cypress/e2e/test/stepdefinitions/CartButtonvalidation.ts
+++
@@ -1,7 +0,0 @@
-import { Given, When, Then, DataTable } from "@badeball/cypress-cucumber-preprocessor";
-import commonPage from "../../pages/commonPage";
-import test from "../../pages/CartbuttonvalidationPage"
-
-When("user click and validate the Cart Button in hompage", () =>{
-test.clickOnDeviceButton()
-})
